bda0edc03b3d7653eb4faaef8ce78728edbd8ea3
H <juana.galan@outlook.com>
2020-06-13 15:11:33 +0100

    Fetches and displays user data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// Get the GitHub username input form
+const gitHubForm = document.getElementById('gitHubForm');
+
+// Listen for submissions on GitHub username input form
+gitHubForm.addEventListener('submit', (e) => {
+    
+    // Prevent default form submission action
+    e.preventDefault();
+
+    // Get the GitHub username input field on the DOM
+    let usernameInput = document.getElementById('usernameInput');
+
+    // Get the value of the GitHub username input field
+    let gitHubUsername = usernameInput.value;          
+
+    // Run GitHub API function, passing in the GitHub username
+    requestUserRepos(gitHubUsername);
+
+})
+
+
+function requestUserRepos(username){
+    
+    // Create new XMLHttpRequest object
+    const xhr = new XMLHttpRequest();
+    
+    // GitHub endpoint, dynamically passing in specified username
+    const url = `https://api.github.com/users/${username}/repos`;
+   
+    // Open a new connection, using a GET request via URL endpoint
+    // Providing 3 arguments (GET/POST, The URL, Async True/False)
+    xhr.open('GET', url, true);
+    
+    // When request is received
+    // Process it here
+    xhr.onload = function () {
+    
+        // Parse API data into JSON
+        const data = JSON.parse(this.response);
+        
+        // Loop over each object in data array
+        for (let i in data) {
+
+            // Get the img with id of of userAvatar
+            let img = document.getElementById('userAvatar');
+
+            // Get image
+            img.innerHTML = (`
+                <img src="${data[0].owner.avatar_url}" />
+            `);
+
+            // Get the ul with id of of userRepos
+            let ul = document.getElementById('userRepos');
+    
+            // Create variable that will create li's to be added to ul
+            let li = document.createElement('li');
+            
+            // Add Bootstrap list item class to each li
+            li.classList.add('list-group-item')
+
+            if (data[i].language !== null && data[i].fork === false) {
+                // Create the html markup for each li
+                li.innerHTML = (`
+                    <p><strong>Repo:</strong> ${data[i].name}</p>
+                    <p><strong>Description:</strong> ${data[i].description}</p>
+                    <p><strong>Language:</strong> ${data[i].language}</p>
+                    <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
+                `);
+            }
+            
+            // Append each li to the ul
+            ul.appendChild(li);
+        
+        }
+
+    }
+    
+    // Send the request to the server
+    xhr.send();
+    
+}
+
 function App() {
+  // Call function passing in GitHub username
+  // requestUserRepos('the-one-and-only-h');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" />
   );
 }
 
